100a3972b36ea8771d953d77a2014e3af2cb3c81
João Victor de Lima Martins <jvictor2012@outlook.com>
2021-07-31 20:18:53 -0300

    Commit site-app-core

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faCode from '@fortawesome/fontawesome-free-solid/faCode'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            {/*<span className="icon fa-diamond"></span>*/}
+            <FontAwesomeIcon icon={faCode} transform="grow-18" />
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Victor Lima</h1>
+                <p>Web Developer</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Serviços</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Sobre</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contato</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('skills')}}>Habilidades</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
